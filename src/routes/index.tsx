0c485cf642b449bf7c8ed9d782a2b8efa0a8dce7
Bruno <bruno3p33@gmail.com>
2022-11-03 13:26:41 -0300

    feat:add login page logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from "react-router";
+import Login from "../pages/Login";
 
 const RoutesMain = () => {
 
     return (
         <Routes>
             <Route path="*" element={<Navigate to="/landing"/>}/>
-            <Route path="/login" element={<h1>login</h1>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<h1>register</h1>}/>
             <Route path="/landing" element={<h1>landing Page</h1>}/>
 
