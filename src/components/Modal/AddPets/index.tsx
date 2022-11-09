0283a62aed1631b76fbdd58e19d894d0fb25b603
Bruno <bruno3p33@gmail.com>
2022-11-09 12:28:24 -0300

    feat/SchemaAddpet

--- src/components/Modal/AddPets/index.tsx
+++ src/components/Modal/AddPets/index.tsx
@@ -4,6 +4,8 @@ import { ModalContext } from "../../../contexts/ModalContext";
 import { petsContext } from "../../../contexts/PetsContext";
 import Modal from "react-modal";
 import { UserContext } from "../../../contexts/UserContext";
+import { addPetSchema } from "../../../validations/createPetSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface iaddFormPet {
   id: number;
@@ -23,7 +25,9 @@ export const ModalAddPets = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<iaddFormPet>({});
+  } = useForm<iaddFormPet>({
+    resolver: yupResolver(addPetSchema),
+  });
 
   const submit = (data: iaddFormPet) => {
     const newData = { ...data, userId: user?.id };
@@ -37,9 +41,13 @@ export const ModalAddPets = () => {
         <form onSubmit={handleSubmit(submit)}>
           <button onClick={() => closeModaladdpet()}>Fechar</button>
           <input type="name" placeholder="Nome" {...register("name")} />
+          {errors.name?.message}
           <input type="type" placeholder="Tipo" {...register("type")} />
+          {errors.name?.message}
           <input type="picture" placeholder="Foto" {...register("picture")} />
+          {errors.name?.message}
           <input type="race" placeholder="Raça" {...register("race")} />
+          {errors.name?.message}
           <button type="submit">Criar novo pet</button>
         </form>
       </Modal>
